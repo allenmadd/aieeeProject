a1f8fb4b197f97a70e97d59f83663f146a38f1f9
allenmadd <mallen@umail.ucsb.edu>
2019-05-11 11:58:36 -0700

    updated structure

---
+++ python3/classifyEnemies.py
@@ -0,0 +1,33 @@
+#
+#Rogue Model 
+#aka what to do in the instance that the closest player is rogue
+#
+
+
+from colorfight import Colorfight
+import time
+import random
+from colorfight.constants import BLD_GOLD_MINE, BLD_ENERGY_WELL, BLD_FORTRESS
+
+import numpy as np
+from io import BytesIO
+from keras.layers import Input,BatchNormalization,Conv2D, Activation
+from keras.layers.advanced_activations import LeakyReLU
+from keras.models import Model
+from keras.utils.vis_utils import plot_model
+from keras.optimizers import Adam
+import os
+
+
+
+
+
+
+
+
+
+
+
+
+
+

943765b985ccbc37d1f737aabfc76a58a55db5bf
charlespetkovich <charlespetkovich@umail.ucsb.edu>
2019-05-11 16:31:34 -0700

    new

--- python3/move.py
+++ python3/move.py
@@ -10,11 +10,23 @@ import time
 import random
 import numpy as np
 from colorfight.constants import BLD_GOLD_MINE, BLD_ENERGY_WELL, BLD_FORTRESS
-import classify
+from classify import Classification
+from assessField import assessField
 
-def allocate(enemyClassifications, tileClassifications, selfBot, game):
-    enemyClassifications = Classify()
+def allocate(enemyClassifications, tileClassifications, selfBot, game, users):
+    
+        
 
-    totWeight = sum(enemyClassifications.aggressive)
 
-def move(enemyClassifications, tileClassifications, selfBot, game):
+def move(selfBot, game):
+    attackDict = assessField(game)[0]
+    enemyClassifications = Classification(users,selfBot.username)
+    if(enemyClassifications.riskOfAttack > 1):
+        Defend
+    elif((enemyClassifications.benefitOfBlitz -enemyClassifications.goodness) > 1):
+        maxReward = 0
+        best = -1
+        for i in attackDict.keys:
+            if(attackDict[i][0] - attackDict[i][1]) > maxReward:
+                maxReward = attackDict[i][0] - attackDict[i][1]
+                best = i
